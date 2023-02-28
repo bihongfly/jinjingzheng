bc64b47ba103a4372ee68494a5bf2256050cf0ad
bihongfei <bihongfei@lixiang.com>
2023-02-28 14:13:46 +0800

    修复了一些已知问题

--- jinjingzheng.py
+++ jinjingzheng.py
@@ -13,12 +13,39 @@ class AutoRenewTrafficPermit(object):
         self.inster_apply_record_url = f"https://{url}/pro/applyRecordController/insertApplyRecord"
         # 访问凭证，通过抓包在请求头信息 Authorization 字段
         self.auth = ""
-
-    def log_info(self, msg):
-        print(f"\033[01;34m{msg}\033[0m")
-
-    def log_err(self, msg):
-        print(f"\033[01;31m{msg}\033[0m")
+        # 续签接口数据只展示部分说明，其他的需要自行抓包研究
+        # 车主姓名
+        user_name = ""
+        # 车主身份证号
+        id_num = ""
+        # 车牌号
+        plate_num = ""
+        # 进京地址
+        address = ""
+        # 以申请明天开始为例
+        tomorrow = (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d")
+        self.payload = {
+            "sqdzgdjd" : "",
+            "txrxx" : [
+            ],
+            "hpzl" : "",
+            "applyIdOld" : "",
+            "sqdzgdwd" : "",
+            "jjmdmc" : "其它",
+            "jsrxm" : user_name,
+            "jszh" : id_num,
+            "jjdq" : "",
+            "jjmd" : "",
+            "sqdzbdjd" : "",
+            "sqdzbdwd" : "",
+            "jjzzl" : "",
+            "jjlk" : "",
+            "hphm" : plate_num,
+            "vId" : "",
+            "jjrq" : tomorrow,
+            "jjlkmc" : "其他道路",
+            "xxdz" : address
+        }
 
     def request(self, url, payload):
         headers = {
@@ -28,65 +55,47 @@ class AutoRenewTrafficPermit(object):
         res_result_json = response.json()
         return res_result_json
 
-    def get_remaining_time(self):
+    def getRemainingTime(self):
         payload = {}
         state_result_json = self.request(self.state_url, payload)
         status_code = state_result_json["code"]
         msg = state_result_json["msg"]
-        self.log_info(f"查询剩余天数状态码：{status_code}")
-        self.log_info(f"查询剩余天数返回信息：{msg}")
+        print(f"查询剩余天数状态码：{status_code}")
+        print(f"查询剩余天数返回信息：{msg}")
         if status_code != 200:
-            self.log_err("返回状态码不等于200，接口异常")
+            print("返回状态码不等于200，接口异常")
             sys.exit()
         current_state = state_result_json["data"]["bzclxx"][0]["bzxx"][0]["blztmc"]
         remaining_time = state_result_json["data"]["bzclxx"][0]["bzxx"][0]["sxsyts"]
         apply_id_old = state_result_json["data"]["bzclxx"][0]["bzxx"][0]["applyId"]
+        print(f"进京证当前状态：{current_state}")
+        print(f"剩余天数：{remaining_time}")
         return current_state,remaining_time,apply_id_old
 
-    def auto_renew(self):
+    def autoRenew(self, payload):
+        renew_result_json = self.request(self.inster_apply_record_url, payload)
+        status_code = renew_result_json["code"]
+        msg = renew_result_json["msg"]
+        print(f"续签接口状态码：{status_code}")
+        print(f"续签接口响应消息：{msg}")
+        state_result_json = self.request(self.state_url,payload = {})
+        if state_result_json["data"]["bzclxx"][0]["ecbzxx"]:
+            current_state = state_result_json["data"]["bzclxx"][0]["ecbzxx"][0]["blztmc"]
+            validity_period = state_result_json["data"]["bzclxx"][0]["ecbzxx"][0]["yxqs"] + "~" + state_result_json["data"]["bzclxx"][0]["ecbzxx"][0]["yxqz"]
+            print(f"申请状态：{current_state}")
+            print(f"生效时间：{validity_period}")
+
+    def main(self):
         current_state,remaining_time,apply_id_old = self.getRemainingTime()
-        self.log_info(f"进京证当前状态：{current_state}")
-        self.log_info(f"剩余天数：{remaining_time}")
+        self.payload["applyIdOld"] = apply_id_old
+        payload = json.dumps(self.payload)
         if current_state == "审核通过(生效中)":
-            self.log_info("生效中，无需重新申请")
+            print("生效中，无需重新申请")
             if remaining_time > 1:
                 sys.exit()
-            self.log_info("剩余时间小于1天，执行续签")
-            tomorrow = (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d")
-            # 防止恶意访问，请求接口参数不做解释，需要的自行抓包
-            payload = json.dumps({
-                "sqdzgdjd" : "",
-                "txrxx" : [
-                ],
-                "hpzl" : "",
-                "applyIdOld" : apply_id_old,
-                "sqdzgdwd" : "",
-                "jjmdmc" : "",
-                "jsrxm" : "",
-                "jszh" : "",
-                "jjdq" : "",
-                "jjmd" : "",
-                "sqdzbdjd" : "",
-                "sqdzbdwd" : "",
-                "jjzzl" : "",
-                "jjlk" : "",
-                "hphm" : "",
-                "vId" : "",
-                "jjrq" : tomorrow,
-                "jjlkmc" : "",
-                "xxdz" : ""
-            })
-            renew_result_json = self.request(self.inster_apply_record_url, payload)
-            status_code = renew_result_json["code"]
-            msg = renew_result_json["msg"]
-            self.log_info(f"续签接口状态码：{status_code}")
-            self.log_info(f"续签接口响应消息：{msg}")
-            state_result_json = self.request(self.state_url,payload = {})
-            if state_result_json["data"]["bzclxx"][0]["ecbzxx"]:
-                current_state = state_result_json["data"]["bzclxx"][0]["ecbzxx"][0]["blztmc"]
-                remaining_time = state_result_json["data"]["bzclxx"][0]["ecbzxx"][0]["sxsyts"]
-                validity_period = state_result_json["data"]["bzclxx"][0]["ecbzxx"][0]["yxqs"] + "~" + state_result_json["data"]["bzclxx"][0]["ecbzxx"][0]["yxqz"]
-                self.log_info(f"申请状态：{current_state}")
-                self.log_info(f"生效时间：{validity_period}")
+            print("剩余时间小于1天，执行续签")
+            self.autoRenew(payload)
+        else:
+            self.autoRenew(payload)
 
-AutoRenewTrafficPermit().auto_renew()
+AutoRenewTrafficPermit().main()
