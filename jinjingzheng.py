793e6147f95850d706915ca4ff5cca09975b048b
bihongfei <bihongfei@lixiang.com>
2023-05-10 15:09:59 +0800

    修复了一些已知问题

--- jinjingzheng.py
+++ jinjingzheng.py
@@ -50,28 +50,28 @@ class AutoRenewTrafficPermit(object):
     def request(self, url, payload):
         headers = {
             "Authorization": self.auth,
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json"
         }
         response = requests.request("POST", url, headers=headers, data=payload)
         res_result_json = response.json()
         return res_result_json
 
     def getRemainingTime(self):
-        payload = {}
-        state_result_json = self.request(self.state_url, payload)
+        state_result_json = self.request(self.state_url,payload = {})
         status_code = state_result_json["code"]
         msg = state_result_json["msg"]
-        print(f"查询剩余天数状态码：{status_code}")
-        print(f"查询剩余天数返回信息：{msg}")
         if status_code != 200:
-            print("返回状态码不等于200，接口异常")
+            print(f"状态码不等于200，接口异常，状态码：{status_code}，错误信息：{msg}")
             sys.exit()
-        current_state = state_result_json["data"]["bzclxx"][0]["bzxx"][0]["blztmc"]
-        remaining_time = state_result_json["data"]["bzclxx"][0]["bzxx"][0]["sxsyts"]
+        # 已经申请续签后，会有ecbzxx字段的数据显示审核通过(待生效)、以及最新的截止日期
+        if state_result_json["data"]["bzclxx"][0]["ecbzxx"]:
+            current_state = state_result_json["data"]["bzclxx"][0]["ecbzxx"][0]["blztmc"]
+            validity_period = state_result_json["data"]["bzclxx"][0]["ecbzxx"][0]["yxqz"]
+        else:
+            current_state = state_result_json["data"]["bzclxx"][0]["bzxx"][0]["blztmc"]
+            validity_period = state_result_json["data"]["bzclxx"][0]["bzxx"][0]["yxqz"]
         apply_id_old = state_result_json["data"]["bzclxx"][0]["bzxx"][0]["applyId"]
-        print(f"进京证当前状态：{current_state}")
-        print(f"剩余天数：{remaining_time}")
-        return current_state,remaining_time,apply_id_old
+        return current_state,validity_period,apply_id_old
 
     def autoRenew(self, payload):
         renew_result_json = self.request(self.inster_apply_record_url, payload)
@@ -79,24 +79,18 @@ class AutoRenewTrafficPermit(object):
         msg = renew_result_json["msg"]
         print(f"续签接口状态码：{status_code}")
         print(f"续签接口响应消息：{msg}")
-        state_result_json = self.request(self.state_url,payload = {})
-        if state_result_json["data"]["bzclxx"][0]["ecbzxx"]:
-            current_state = state_result_json["data"]["bzclxx"][0]["ecbzxx"][0]["blztmc"]
-            validity_period = state_result_json["data"]["bzclxx"][0]["ecbzxx"][0]["yxqs"] + "~" + state_result_json["data"]["bzclxx"][0]["ecbzxx"][0]["yxqz"]
-            print(f"申请状态：{current_state}")
-            print(f"生效时间：{validity_period}")
 
     def main(self):
-        current_state,remaining_time,apply_id_old = self.getRemainingTime()
+        current_state,validity_period,apply_id_old = self.getRemainingTime()
         self.payload["applyIdOld"] = apply_id_old
         payload = json.dumps(self.payload)
         if current_state == "审核通过(生效中)":
-            print("生效中，无需重新申请")
-            if remaining_time > 1:
-                sys.exit()
-            print("剩余时间小于1天，执行续签")
-            self.autoRenew(payload)
+            today = datetime.now().strftime("%Y-%m-%d")
+            if validity_period == today:
+                print("剩余时间小于1天，执行续签")
+                self.autoRenew(payload)
+        elif current_state == "审核通过(待生效)":
+            print("审核通过(待生效),无需重新申请")
         else:
             self.autoRenew(payload)
-
 AutoRenewTrafficPermit().main()
